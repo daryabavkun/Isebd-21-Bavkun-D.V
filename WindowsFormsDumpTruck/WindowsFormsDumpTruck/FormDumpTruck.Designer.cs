7462fb786586c6fab83adc9f9c8706f38dbde49d
Дарья Бавкун <bavkun@mail.ru>
2018-12-06 23:26:09 +0400

    laba1

---
+++ WindowsFormsDumpTruck/WindowsFormsDumpTruck/FormDumpTruck.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsDumpTruck
+{
+    partial class FormTruck
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreateTruck = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.pictureBoxDumpTruck = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDumpTruck)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCreateTruck
+            // 
+            this.buttonCreateTruck.Location = new System.Drawing.Point(16, 15);
+            this.buttonCreateTruck.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonCreateTruck.Name = "buttonCreateTruck";
+            this.buttonCreateTruck.Size = new System.Drawing.Size(100, 28);
+            this.buttonCreateTruck.TabIndex = 1;
+            this.buttonCreateTruck.Text = "создать ";
+            this.buttonCreateTruck.UseVisualStyleBackColor = true;
+            this.buttonCreateTruck.Click += new System.EventHandler(this.buttonCreateTruck_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = global::WindowsFormsDumpTruck.Properties.Resources.лево;
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(1047, 473);
+            this.buttonLeft.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(40, 37);
+            this.buttonLeft.TabIndex = 5;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = global::WindowsFormsDumpTruck.Properties.Resources.низ;
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(1092, 517);
+            this.buttonDown.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(40, 37);
+            this.buttonDown.TabIndex = 4;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackgroundImage = global::WindowsFormsDumpTruck.Properties.Resources.низ1;
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(1139, 473);
+            this.buttonRight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(40, 37);
+            this.buttonRight.TabIndex = 3;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = global::WindowsFormsDumpTruck.Properties.Resources.верх1;
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(1092, 428);
+            this.buttonUp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(40, 37);
+            this.buttonUp.TabIndex = 2;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // pictureBoxDumpTruck
+            // 
+            this.pictureBoxDumpTruck.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBoxDumpTruck.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxDumpTruck.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxDumpTruck.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBoxDumpTruck.Name = "pictureBoxDumpTruck";
+            this.pictureBoxDumpTruck.Size = new System.Drawing.Size(1179, 569);
+            this.pictureBoxDumpTruck.TabIndex = 0;
+            this.pictureBoxDumpTruck.TabStop = false;
+            this.pictureBoxDumpTruck.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // FormTruck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 569);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonCreateTruck);
+            this.Controls.Add(this.pictureBoxDumpTruck);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormTruck";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Самосвал";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDumpTruck)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDumpTruck;
+        private System.Windows.Forms.Button buttonCreateTruck;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonLeft;
+    }
+}
+
---
+++ WindowsFormsDumpTruck/WindowsFormsDumpTruck/FormDumpTruck.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDumpTruck
+{
+    public partial class FormTruck : Form
+    {
+        private DumpTruck dumptruck;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public FormTruck()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Метод отрисовки грузовика
+        /// </summary>
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxDumpTruck.Width, pictureBoxDumpTruck.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            dumptruck.DrawTruck(gr);
+            pictureBoxDumpTruck.Image = bmp;
+        }
+        /// <summary>
+        /// Обработка нажатия кнопки "Создать"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCreateTruck_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            dumptruck = new DumpTruck(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue, Color.Yellow);
+            dumptruck.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxDumpTruck.Width,
+           pictureBoxDumpTruck.Height);
+            Draw();
+        }
+        /// <summary>
+        /// Обработка нажатия кнопок управления
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    dumptruck.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    dumptruck.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    dumptruck.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    dumptruck.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+
+        }
+    }
+}
